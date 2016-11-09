09ce2fa088f587aed1286dfa81e3bc5d0bf2fb69
Chris Bendel <chrissbendel@gmail.com>
2016-11-09 14:07:00 -0500

    first commit

---
+++ groups.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <grp.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+char* groupNameFromId(gid_t gid) {
+	struct group *grp;
+	grp = getgrgid(gid);
+	return (grp == NULL) ? NULL : grp->gr_name;
+}
+
+gid_t groupIdFromName(const char *name) {
+	struct group *grp;
+	gid_t  g;
+	char *endptr;
+
+	if (name == NULL || *name == '\0')
+		return -1;
+
+	g = strtol(name, &endptr, 10);
+	if (*endptr == '\0') 
+		return g;
+	grp = getgrnam(name);
+	if (grp == NULL)
+		return -1;
+	return grp->gr_gid;
+}
+
+int main(int argc, char *argv[])
+{
+	struct passwd *p = getpwuid(getuid());
+	int j, i;
+	gid_t *groups;
+	struct group *g;
+	g = getgrgid(groups[0]);	
+	printf("Group name: %s", g->gr_name);
+	if (argc == 1)
+	{
+		g = getgrgid(p->pw_gid);
+		printf("User: %s\n", p->pw_name);	
+		printf("GroupID: %u GroupName: %s\n", p->pw_gid, g->gr_name);	
+	}
+	else {
+		for (i = 1; i < argc; i++)
+		{
+			int grpID = groupIdFromName(argv[i]);
+		
+			printf("Name: %s GroupId: %d \n", argv[i], grpID);
+		}
+	}		
+}
+
