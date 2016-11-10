b58d2e28717d4f22f58ad0db7a0c025559214837
Chris Bendel <chrissbendel@gmail.com>
2016-11-10 08:18:59 -0500

    loop over and output users

--- groups.c
+++ groups.c
@@ -5,50 +5,58 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-char* groupNameFromId(gid_t gid) {
-	struct group *grp;
-	grp = getgrgid(gid);
-	return (grp == NULL) ? NULL : grp->gr_name;
-}
-
-gid_t groupIdFromName(const char *name) {
-	struct group *grp;
-	gid_t  g;
-	char *endptr;
-
-	if (name == NULL || *name == '\0')
-		return -1;
-
-	g = strtol(name, &endptr, 10);
-	if (*endptr == '\0') 
-		return g;
-	grp = getgrnam(name);
-	if (grp == NULL)
-		return -1;
-	return grp->gr_gid;
-}
-
-int main(int argc, char *argv[])
-{
-	struct passwd *p = getpwuid(getuid());
-	int j, i;
+int main(int argc, char *argv[]) {
+	int i = 1, j, maxNumGroups = 32;
 	gid_t *groups;
-	struct group *g;
-	g = getgrgid(groups[0]);	
-	printf("Group name: %s", g->gr_name);
-	if (argc == 1)
-	{
-		g = getgrgid(p->pw_gid);
-		printf("User: %s\n", p->pw_name);	
-		printf("GroupID: %u GroupName: %s\n", p->pw_gid, g->gr_name);	
-	}
-	else {
-		for (i = 1; i < argc; i++)
-		{
-			int grpID = groupIdFromName(argv[i]);
-		
-			printf("Name: %s GroupId: %d \n", argv[i], grpID);
+	struct passwd *pw;
+	struct group *gr;
+	char *name;
+
+	for (i = 1; i < argc; i++) {
+		name = argv[i];
+		groups = malloc(maxNumGroups * sizeof (gid_t));
+		pw = getpwnam(name);
+
+		if (pw == NULL) {
+				printf("%s is not a user\nTry searching again \n", argv[i]);
+				exit(EXIT_FAILURE);
 		}
-	}		
+
+		if (getgrouplist(name, pw->pw_gid, groups, &maxNumGroups) == -1) {
+				printf("Cant find groups \n");
+		}
+
+		printf("%s: ", name);
+		for (j = 0; j < maxNumGroups; j++) {
+			// printf("%d", groups[j]);
+			gr = getgrgid(groups[j]);
+			if (gr != NULL)
+				printf(" (%s)", gr->gr_name);
+		}
+		printf("\n");
+	}
 }
 
+
+// char* groupNameFromId(gid_t gid) {
+// 	struct group *grp;
+// 	grp = getgrgid(gid);
+// 	return (grp == NULL) ? NULL : grp->gr_name;
+// }
+//
+// gid_t groupIdFromName(const char *name) {
+// 	struct group *grp;
+// 	gid_t  g;
+// 	char *endptr;
+//
+// 	if (name == NULL || *name == '\0')
+// 		return -1;
+//
+// 	g = strtol(name, &endptr, 10);
+// 	if (*endptr == '\0')
+// 		return g;
+// 	grp = getgrnam(name);
+// 	if (grp == NULL)
+// 		return -1;
+// 	return grp->gr_gid;
+// }
