649566fe39109b2b23974159179005479f160b59
Chris Bendel <chrissbendel@gmail.com>
2016-11-16 15:37:06 -0500

    check argvs

--- ls.c
+++ ls.c
@@ -1,2 +1,42 @@
-#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 
+int main(int argc, char* argv[])
+{
+    const char *path = ".";
+    DIR *d;
+    struct dirent *dir;
+    struct stat info;
+    d = opendir(path);
+
+    if (d == NULL) {
+      printf("error\n");
+      return 0;
+    }
+
+    if (argc == 1) {
+      while ((dir = readdir(d)) != NULL) {
+        if (dir->d_name[0] != '.') {
+          printf("%s\n", dir->d_name);
+        }
+      }
+    }
+    for (int i=1; i < argc; i++) {
+      if ((strncmp(argv[i], "-l", 2)) == 0) {
+
+      }
+      if ((strncmp(argv[i], "-a", 2)) == 0) {
+        while ((dir = readdir(d)) != NULL) {
+          printf("%s\n", dir->d_name);
+        }
+      }
+      if (argv[i] == "-la" || argv[i] == "-al") {
+
+      }
+    }
+}
