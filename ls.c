962eda214e7f59ce5eaf3260f88cdbf87ccc6adc
Chris Bendel <chrissbendel@gmail.com>
2016-11-16 21:29:22 -0500

    working

--- ls.c
+++ ls.c
@@ -1,42 +1,119 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 
+void longFormat (char *name, const char *path) {
+	struct stat fileInfo;
+	char appendedPath[512];
+	sprintf(appendedPath, "%s/%s", path, name);
+	stat(appendedPath, &fileInfo);
+
+	printf( (S_ISDIR(fileInfo.st_mode)) ? "d" : "-");
+	printf( (fileInfo.st_mode & S_IRUSR) ? "r" : "-");
+	printf( (fileInfo.st_mode & S_IWUSR) ? "w" : "-");
+	printf( (fileInfo.st_mode & S_IXUSR) ? "x" : "-");
+	printf( (fileInfo.st_mode & S_IRGRP) ? "r" : "-");
+	printf( (fileInfo.st_mode & S_IWGRP) ? "w" : "-");
+	printf( (fileInfo.st_mode & S_IXGRP) ? "x" : "-");
+	printf( (fileInfo.st_mode & S_IROTH) ? "r" : "-");
+	printf( (fileInfo.st_mode & S_IWOTH) ? "w" : "-");
+	printf( (fileInfo.st_mode & S_IXOTH) ? "x" : "-");
+
+	printf(" %d", fileInfo.st_nlink);
+
+	struct passwd *pwd;
+	if ((pwd = getpwuid(fileInfo.st_uid)) != NULL)
+    printf(" %s", pwd->pw_name);
+
+	struct group *grp;
+	if ((grp = getgrgid(fileInfo.st_gid)) != NULL)
+		printf(" %s", grp->gr_name);
+
+	printf(" %lld", fileInfo.st_size);
+
+	struct tm ts;
+	ts = *localtime(&fileInfo.st_mtime);
+	char date[80];
+	strftime(date, sizeof(date), "%b %d %H:%M", &ts);
+	printf(" %s", date);
+  // printf(" %s", name);
+}
+
+/*------------------------------*/
+
 int main(int argc, char* argv[])
 {
     const char *path = ".";
     DIR *d;
     struct dirent *dir;
-    struct stat info;
     d = opendir(path);
+    char buf[512];
+    char *allFiles[512];
+    char *hFiles[512];
+    char *rFiles[512];
+    int count = 0;
+    int rCount = 0, hCount = 0;
 
     if (d == NULL) {
       printf("error\n");
       return 0;
     }
 
-    if (argc == 1) {
-      while ((dir = readdir(d)) != NULL) {
-        if (dir->d_name[0] != '.') {
-          printf("%s\n", dir->d_name);
-        }
+    //store every file into array
+    //separate hidden and regular files in different arrays
+    while ((dir = readdir(d)) != NULL) {
+      allFiles[count] = dir->d_name;
+      if (allFiles[count][0] == '.') {
+        hFiles[hCount] = allFiles[count];
+        hCount++;
       }
+      if (allFiles[count][0] != '.') {
+        rFiles[rCount] = allFiles[count];
+        rCount++;
+      }
+      count++;
     }
-    for (int i=1; i < argc; i++) {
-      if ((strncmp(argv[i], "-l", 2)) == 0) {
 
-      }
-      if ((strncmp(argv[i], "-a", 2)) == 0) {
-        while ((dir = readdir(d)) != NULL) {
-          printf("%s\n", dir->d_name);
+    switch (argc) {
+      case 1:
+        for (int i = 0; i < rCount; i++) {
+          printf("%s\n", rFiles[i]);
         }
-      }
-      if (argv[i] == "-la" || argv[i] == "-al") {
-
-      }
+        break;
+      case 2:
+        //prints all in long format
+        if ((strncmp(argv[1], "-la", 3) == 0 ) || ((strncmp(argv[1], "-al", 3)) == 0)) {
+          for (int i = 0; i < count; i++) {
+            longFormat(allFiles[i], path);
+            printf(" %s\n", allFiles[i]);
+          }
+          return 1;
+        }
+        //prints long format for non hidden files
+        if ((strncmp(argv[1], "-l", 2)) == 0) {
+          for (int i = 0; i < rCount; i++) {
+            longFormat(rFiles[i], path);
+            printf(" %s", rFiles[i]);
+            printf("\n");
+          }
+          return 1;
+        }
+        //prints all files including hidden
+        if ((strncmp(argv[1], "-a", 2)) == 0) {
+          for (int i = 0; i < count; i++) {
+            printf("%s\n", allFiles[i]);
+          }
+          return 1;
+        }
+        break;
+      case 3:
+        break;
     }
 }
