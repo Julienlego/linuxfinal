12a58756cd460935a7450550fef1b3173f1cff19
Julien Gilbert <julienlego@gmail.com>
2016-12-07 14:32:53 -0500

    fixed
    it works

--- cd.c
+++ cd.c
@@ -4,34 +4,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <pwd.h>
+#include <limits.h>
 
 int main(int argc, char *argv[]) {
-	
-	const char *path;
-	uid_t uid = getuid();
-	struct passwd* pwd = getpwuid(uid);
-	
-	if (!pwd)
-    {
-        printf("User with %u ID is unknown.\n", uid);
-        return -1;
-    }
-
-    printf("User '%s' has home directory at '%s'\n", pwd->pw_name, pwd->pw_dir);
+	char *cwd;
+	char buff[PATH_MAX + 1];
 
 	if (argc == 1) {
 		printf("changing directory to HOME...\n");
-		//chdir(getenv("HOME"));
-		if (chdir(pwd->pw_dir) != 0)
-	    {
-	        printf("Unable to change directory to '%s'\n", pwd->pw_dir);
-	        return -1;
-	    }
+		chdir(getenv("HOME"));
 	}
 	else {
 		printf("changing directory to path...\n");
 		chdir(argv[1]);
 	}
+	
+	cwd = getcwd( buff, PATH_MAX + 1);
+	if( cwd != NULL ) {
+        printf( "My working directory is %s.\n", cwd );
+    }
+	
 	return 0;
 }
