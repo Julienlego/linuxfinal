3a43c1d717aa43c2d3ffec3d926324774a64bac1
Julien Gilbert <julienlego@gmail.com>
2016-12-16 19:32:54 -0500

    Update bsh.c

--- bsh.c
+++ bsh.c
@@ -1,51 +1,41 @@
 /* Chris Bendel, Julien Gilbert */
 
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    char *line = NULL;    // input pre-parse
+char ** parseInput(char *input);
+char * readInput(void);
+int cmdLaunch(char *cmd, char **args);
+int cmdExecute(char *line, char **input);
+
+
+int main(int argc, char **argv) {
+    char *line;    // input pre-parse
     char **input;       // input parsed
-    pid_t pid;
+    int status;
     
-    /* asks user for input till they exit*/
-    while (input[0] != "exit" || input[0] != "EXIT") {
+    do
+    {
+        printf("bettershell --> ");
         line = readInput();
-        input = parseInput(scan);
-        
-        if( input[0] == "groups") {
-            execpv("./groups", input);
-        }
-        else if(input[0] == "ls") {
-            execpv("./ls", input);
-        }
-        else if(input[0] == "cp") {
-            execpv("./cp", input);
-        }
-        else if(input[0] == "cd") {
-            execpv("./cd", input);
-        }
-        else {
-            system(scan);
-        }
-        pid = fork();
-    }
+        input = parseInput(line);
+        status = cmdExecute(line,input);
+        /* free allocated memory */
+        free(line);
+	    free(input);
+    } while(status == 0);
     
-    /* free allocated memory */
-	free(line);
-	free(input);
-	
 	return EXIT_SUCCESS;
 }
 
 /* parses the user input */
-char **parseInput(char *input)
+char ** parseInput(char *input)
 {
     /* Initialize Variables */
     int pos = 0;
-    char **letters = malloc(64 * sizeof(char));
+    char **letters = malloc(128 * sizeof(char));
     char *letter;
  
     /* Traverse Input */
@@ -66,11 +56,82 @@ char **parseInput(char *input)
 }
 
 /* reads and returns line from user */
-char *readInput()
+char * readInput(void)
 {
     char *line = NULL;
-    size_t bufsize = 0; // allocates a buffer
-    printf("bettershell ---> ");
-    getline(&line, &bufsize, stdin);
+    ssize_t bufsize = 0; // allocates a buffer
+	getline(&line, &bufsize, stdin);
     return line;
+}
+
+/* creates process for command */
+int cmdLaunch(char *cmd, char **args)
+{
+    pid_t pid, wpid;
+    int status;
+    
+    pid = fork();
+    /* error checking */
+    if (pid == 0) {
+        if (execvp(cmd, args) == -1)
+        {
+             perror("Error_Forking");
+        }
+    }
+    else if (pid < 0) {
+        perror("Error_Forking");
+    }
+    else {
+
+    /* parent waits till child is done*/
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+/* determines command and executes it */
+int cmdExecute(char *line, char **input)
+{
+    int i;
+    printf("%s\n", input[0]);
+    if(input[0] == NULL)
+	{
+		return 0;
+	}
+	else if( strcmp(input[0], "groups") == 0)
+	{
+        cmdLaunch("groups", input);
+        return 0;
+    }
+    else if(strcmp(input[0], "ls") == 0) 
+    {
+        cmdLaunch("ls", input);
+        return 0;
+    }
+    else if(strcmp(input[0], "cp") == 0)
+    {
+        cmdLaunch("cp", input);
+        return 0;
+    }
+    else if(strcmp(input[0], "cd") == 0) 
+    {
+        cmdLaunch("cd", input);
+        return 0;
+    }
+    else if(strcmp(input[0], "exit") == 0)
+    {
+        return -1;
+    }
+    else if(system(line) == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        printf("That is not a valid command\n");
+    }
+    return 0;
 }
